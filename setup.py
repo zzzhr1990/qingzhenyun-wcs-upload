3b93a55448bb70a260678a50c79f5b1a95f9ecd1
zzzhr <zzzhr@hotmail.com>
2017-09-05 00:51:03 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys
+import os
+
+version = '0.1.1.0'
+
+setup(name='qingzhenyun-wcs-upload',
+      version=version,
+      description="Qiingzhenyun WCS SDK",
+      long_description="""\
+Qingzhenyun WCS SDK""",
+      classifiers=[],
+      author='zzzhr',
+      author_email='zzzhr@hotmail.com',
+      url='https://github.com/zzzhr1990/qingzhenyun-wcs-upload',
+      license='Apache',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'requests>=2.11',
+          'python-magic>=0.4.1',
+          'six>=1.4.1',
+          'Twisted>=17.5.0',
+          'urllib3>=1.21.1'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
