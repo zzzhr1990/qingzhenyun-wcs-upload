f07910b6b0a1dc69b37864c2720b38fdb30d40f1
zzzhr <zzzhr@hotmail.com>
2018-10-14 01:47:16 +0800

    whart if time

--- wcstoolbox/wcssimpleuploader.py
+++ wcstoolbox/wcssimpleuploader.py
@@ -95,6 +95,7 @@ class WcsSimpleUploader(object):
 
     def _read_and_post(self,block_info):
         logging.warning('Start Post %s', json.dumps(block_info))
+        time.sleep(200)
         with open(self.filepath, 'rb') as input_stream:
             if block_info['offset'] > 0:
                 input_stream.seek(block_info['offset'])
